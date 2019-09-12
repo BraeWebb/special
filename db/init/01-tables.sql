ba0e8b572105fee3ee7f3891243d8027bb0b0600
BraeWebb <email@braewebb.com>
2019-09-12 12:58:35 +1000

    Fixed Databases

--- db/init/01-tables.sql
+++ db/init/01-tables.sql
@@ -1,18 +1,18 @@
 \connect special
 
-CREATE TABLE special.Queue (
-    asker special.User NOT NULL,
+CREATE TABLE IF NOT EXISTS special.User (
+  id integer NOT NULL,
+  name text,
+  questions_today integer,
+  questions_all_time integer,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS special.Queue (
+    asker integer NOT NULL REFERENCES special.User(id),
     time_joined time,
     type text,
     PRIMARY KEY (asker)
 );
 COMMENT ON TABLE special.Queue IS
 'Current queue entries';
-
-CREATE TABLE special.User (
-  id integer NOT NULL,
-  name text,
-  questions_today integer,
-  questions_all_time integer,
-  PRIMARY KEY (id)
-)
---
+++ db/init/02-dummy.sql
@@ -0,0 +1,8 @@
+\connect special
+
+INSERT INTO special.User VALUES (44354006, 'Brae Webb', 2, 70);
+INSERT INTO special.User VALUES (44394918, 'Emily Bennett', 0, 2);
+INSERT INTO special.User VALUES (44341800, 'Henry OBrien', 0, 0);
+
+INSERT INTO special.Queue VALUES (44354006, now(), 'long');
+INSERT INTO special.Queue VALUES (44394918, now(), 'quick');
