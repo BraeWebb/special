5dde2e2aec645c530f29e159341c303c9ed199b7
BraeWebb <email@braewebb.com>
2019-10-08 12:38:52 +1000

    Reports Schema

--- db/init/01-tables.sql
+++ db/init/01-tables.sql
@@ -16,3 +16,54 @@ CREATE TABLE IF NOT EXISTS special.Queue (
 );
 COMMENT ON TABLE special.Queue IS
 'Current queue entries';
+
+CREATE TABLE IF NOT EXISTS special.ReportLanguages (
+	id text NOT NULL,
+	title text NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS special.LanguageExtensions (
+	language text NOT NULL REFERENCES special.ReportLanguages(id),
+	extension text NOT NULL,
+	PRIMARY KEY (language, extension)
+);
+
+CREATE TABLE IF NOT EXISTS special.ReportRequest (
+	id text NOT NULL,
+	title text NOT NULL,
+	file text NOT NULL,
+	language text NOT NULL REFERENCES special.ReportLanguages(id),
+	max_matches integer NOT NULL,
+	max_cases integer NOT NULL,
+	status text,
+	generator integer NOT NULL REFERENCES special.User(id),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS special.Report (
+	id text NOT NULL,
+	title text NOT NULL,
+	url text NOT NULL,
+	request text NOT NULL REFERENCES special.ReportRequest(id),
+	generator integer NOT NULL REFERENCES special.User(id),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS special.Case (
+	id integer NOT NULL,
+	report text NOT NULL REFERENCES special.Report(id),
+	student1 text NOT NULL,
+	student1_percent integer NOT NULL,
+	student2 text NOT NULL,
+	student2_percent integer NOT NULL,
+	lines integer NOT NULL,
+	PRIMARY KEY (id, report)
+);
+
+CREATE TABLE IF NOT EXISTS special.Script (
+	report text NOT NULL REFERENCES special.Report(id),
+	student text NOT NULL,
+	content text,
+	PRIMARY KEY (report, student)
+);
--- db/init/02-dummy.sql
+++ db/init/02-dummy.sql
@@ -6,3 +6,15 @@ INSERT INTO special.User VALUES (44341800, 'Henry OBrien', 0, 0);
 
 INSERT INTO special.Queue VALUES (44354006, now(), 'long');
 INSERT INTO special.Queue VALUES (44394918, now(), 'quick');
+
+INSERT INTO special.ReportLanguages VALUES ('java', 'Java');
+INSERT INTO special.ReportLanguages VALUES ('python', 'Python');
+INSERT INTO special.ReportLanguages VALUES ('c', 'C');
+INSERT INTO special.ReportLanguages VALUES ('cc', 'CC');
+INSERT INTO special.ReportLanguages VALUES ('ml', 'ML');
+INSERT INTO special.ReportLanguages VALUES ('csharp', 'C#');
+INSERT INTO special.ReportLanguages VALUES ('matlab', 'MatLab');
+INSERT INTO special.ReportLanguages VALUES ('vb', 'Visual Basic');
+INSERT INTO special.ReportLanguages VALUES ('javascript', 'JavaScript');
+INSERT INTO special.ReportLanguages VALUES ('ascii', 'ASCII');
+
